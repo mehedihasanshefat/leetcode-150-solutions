a0ac4988c20a7046758ff459382fd51eb16e5ed5
Mehedi Hasan Shefat <webdev.shefat@gmail.com>
2026-06-25 19:36:13 +0600

    Time: 43 ms (96.85%), Space: 57.3 MB (86.79%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
@@ -5,10 +5,10 @@
 var removeDuplicates = function(nums) {
     if (nums.length <= 2) return nums.length;
     
-    let k = 2; // Start from index 2, as the first two elements are always allowed
+    let k = 2;
 
     for (let i = 2; i < nums.length; i++) {
-        // Compare the current element with the element two positions before the insertion point k
+       
         if (nums[i] !== nums[k - 2]) {
             nums[k] = nums[i];
             k++;
