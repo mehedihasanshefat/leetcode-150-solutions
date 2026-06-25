ffaf6b45887916b69d6de8b6b38cce0bacbc5829
Mehedi Hasan Shefat <webdev.shefat@gmail.com>
2026-06-25 19:48:11 +0600

    Time: 0 ms (100%), Space: 54.4 MB (44.61%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -3,14 +3,13 @@
  * @param {number} m
  * @param {number[]} nums2
  * @param {number} n
- * @return {void} Do not return anything, modify nums1 in-place instead.
+ * @return {void}
  */
 var merge = function(nums1, m, nums2, n) {
-    let p1 = m - 1;      // Pointer for the last valid element in nums1
-    let p2 = n - 1;      // Pointer for the last element in nums2
-    let p = m + n - 1;   // Pointer for the last position in nums1
+    let p1 = m - 1;      
+    let p2 = n - 1;      
+    let p = m + n - 1;   
 
-    // Compare elements from the back and move the larger one to the end
     while (p1 >= 0 && p2 >= 0) {
         if (nums1[p1] > nums2[p2]) {
             nums1[p] = nums1[p1];
