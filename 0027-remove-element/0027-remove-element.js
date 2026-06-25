92124b0c0e81a58afee032bffb07d890aad620e0
Mehedi Hasan Shefat <webdev.shefat@gmail.com>
2026-06-25 19:28:38 +0600

    Time: 0 ms (100%), Space: 53.7 MB (59.46%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let k = 0; // Pointer to track the position of elements not equal to val
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
+};
