ea1235102f58452ac25f05c88b1c762bdafc8d5e
Mehedi Hasan Shefat <webdev.shefat@gmail.com>
2026-06-25 19:48:42 +0600

    Time: 3 ms (15%), Space: 56.2 MB (96.29%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -5,10 +5,10 @@
 var removeDuplicates = function(nums) {
     if (nums.length === 0) return 0;
     
-    let k = 1; // Pointer for the next unique element position
+    let k = 1; 
 
     for (let i = 1; i < nums.length; i++) {
-        // If we find a new unique element
+       
         if (nums[i] !== nums[i - 1]) {
             nums[k] = nums[i];
             k++;
